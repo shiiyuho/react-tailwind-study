8a4f4fedcbbb725b0cb4bf2b2b93107d2d58b558
ishiiyuho <yuho.ishii.uhd@gmail.comS>
2024-05-27 15:48:16 +0900

    機能追加（ホバー、アラート）

--- section2/my-app/src/conponent/Button/Button.js
+++ section2/my-app/src/conponent/Button/Button.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 const Button1 = () => {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
     <div>
-      <button className="bg-red-500 text-red font-bold py-2 px-4 rounded">
+      <button
+        className="bg-red-500 text-white font-bold py-2 px-4 rounded hover:bg-red-600"
+        onClick={handleClick}
+      >
         Click me
       </button>
     </div>
